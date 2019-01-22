57d2ed53bbefee9b3b587ec0020906919be88f3b
Chen Yong <cy908@qq.com>
2019-01-22 18:11:17 +0800

    修复创建目录BUG

--- src/main/java/com/xyz/app/image/Compress.java
+++ src/main/java/com/xyz/app/image/Compress.java
@@ -49,9 +49,11 @@ public class Compress {
 				log.error(MessageFormat.format("[{0}]不是文件！", out));
 				return;
 			}
-			if (!file.getParentFile().mkdirs()) {
-				log.error(MessageFormat.format("[{0}]创建失败！", file.getParentFile().getAbsolutePath()));
-				return;
+			if (!file.getParentFile().exists()) {
+				if (!file.getParentFile().mkdirs()) {
+					log.error(MessageFormat.format("[{0}]创建失败！", file.getParentFile().getAbsolutePath()));
+					return;
+				}
 			}
 		} else {
 			if (file.isFile()) {
